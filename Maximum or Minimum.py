3eac1b40742b27f9a31b4e3d71fff084d194453d
SudPY <91254009+SudPY@users.noreply.github.com>
2021-09-30 15:00:52 +0530

    Add files via upload

---
+++ Maximum or Minimum.py
@@ -0,0 +1,15 @@
+print("I am a maximum or minimum bot!")
+
+a = input("Enter first number!")
+b = input("Enter second number!")
+
+if a > b:
+    print(a, "is the maximum of the two numbers")
+    print(b, "is the minimum of the two numbers")
+
+elif a == b:
+    print("Both numbers are equal")
+
+else:
+    print(b, "is the maximum of the two numbers")
+    print(a, "is the minimum of the two numbers")
